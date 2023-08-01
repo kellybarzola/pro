d574eaa5e3d4c2da7bc5db04a2cfaf8c31b680f5
kellybarzola <132022274+kellybarzola@users.noreply.github.com>
2023-07-31 22:17:00 -0500

    Add files via upload

---
+++ PROYECTO/index.js
@@ -0,0 +1,6 @@
+const btncarts =document.querySelector('.container-icon')
+const containerCartProducts = document.querySelector('.container-cart-products')
+
+btncarts.addEventListener('click', () => {
+    containerCartProducts.classList.toggle('hidden-cart')
+})
